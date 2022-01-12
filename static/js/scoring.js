90c8f1df29fdb734f5e2eb527925b70e6df1d7ba
psaly <psaly@uwo.ca>
2022-01-12 01:06:49 -0500

    display team names and number of players

--- static/js/scoring.js
+++ static/js/scoring.js
@@ -7,9 +7,9 @@
 
             $("#tournament-name").html('<h1 class="page-mainheading">' + data.tournament_details.tournament_name + '</h1>')
             $("#course-name").html('<h2 class="tournament-info-h2">'+ data.tournament_details.course_name +'</h2>')
-            $("course-info").html('<h4 class="leaderboard is--left">' + data.tournament_details.course_details + '<br></h4>')
+            $("#course-info").html('<h4 class="leaderboard is--left">' + data.tournament_details.course_details + '<br></h4>')
             $("#tournament-date").html('<h4 class="leaderboard">' + data.tournament_details.dates + '<br></h4>')
-
+            
             $.each(data.teams, function(_, team) {
                 // #m0-p0-info
                 let matchup_info_div = `#${data.matchup_base_ids[team.manager]}-info`;
@@ -24,6 +24,12 @@
                     '</h2></div></div>';
                     
                 $(matchup_info_div).html(info_html);
+
+                let team_name_div = `#${data.matchup_base_ids[team.manager]}-team`
+                let team_num_players_div = `#${data.matchup_base_ids[team.manager]}-players`
+
+                $(team_name_div).html('<h4 class="scoring-breakdown-h4 is--left">' + team["team_name"] + '</h4>')
+                $(team_num_players_div).html('<h4 class="scoring-breakdown-h4 is--right">' + team["players"].length + ' Players</h4>')
 
                 $.each(team.players, function(_, player) {
                     let player_score = format_score(player.score_to_par)
@@ -41,7 +47,7 @@
                         '</h4></div></div>';
                     // #m0-p0-table
                     let matchup_table_div = `#${data.matchup_base_ids[team.manager]}-table`;
-                    console.log(matchup_table_div);
+                    // console.log(matchup_table_div);
                     $(matchup_table_div).append(score_html);
                 });
             });
